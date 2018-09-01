5a59aafdcd6bbbed10b7ab55e8149e185ad707da
Rom <skav@gmx.fr>
2018-09-01 16:28:31 +0200

    Update config.sh to 17000

--- config.sh
+++ config.sh
@@ -41,9 +41,9 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "--------------------------------"
-  ui_print "ExSDCard Write Access Enabler v3.4"
+  ui_print "ExSDCard Write Access Enabler v3.5"
   ui_print "with Oreo support"
-  ui_print "For Magisk v15+"
+  ui_print "For Magisk v17+"
   ui_print "By Rom"
   ui_print "--------------------------------"
 }
